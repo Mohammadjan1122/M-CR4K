35ea18341e8f5dd7c71b629e83c7989a8227e2d6
Mohammadjan1122 <63337310+Mohammadjan1122@users.noreply.github.com>
2022-02-10 23:42:52 +0430

    Add files via upload

---
+++ afg.py
@@ -0,0 +1,15 @@
+#Author Mohammad sultani
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from mking import mohammad
+    mohammad()
+elif bit == '32bit':
+    from mking import mohammad
+    mohammad()
