2ad104465b939de8921843a9b78d5d1f524ccea7
Mohammadjan1122 <63337310+Mohammadjan1122@users.noreply.github.com>
2022-03-15 00:33:01 +0430

    Create afg.py

---
+++ aarch64/afg.py
@@ -0,0 +1,18 @@
+#Author Mohammad sultani
+
+import os
+
+os.system("git pull")
+
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+from mking import start_mking 
+
+start_mking()
+
